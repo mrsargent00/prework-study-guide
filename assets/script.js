0a896d63bae61b6815c41b19dcf132cd72d600ce
Fluffy <mrsargent00@gmail.com>
2023-07-21 20:17:55 -0400

    Added loops and Arrays

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript']
 
 for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
+    console.log(topics[x]);
+   }
 
 /*
 if (topic === 'HTML') {
- console.log("Let's study HTML!");
+  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
- console.log("Let's study CSS!");
+  console.log("Let's study CSS!");
 } else if (topic === 'Git') {
- console.log("Let's study Git!");
+  console.log("Let's study Git!");
 } else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
+  console.log("Let's study JavaScript!");
 } else {
- console.log('Please try again!');
+  console.log('Please try again!');
 }
 */
